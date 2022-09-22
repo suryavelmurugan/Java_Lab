a552b6f088ada8b5c866957d950e93a649253e69
suryavelmurugan <113089136+suryavelmurugan@users.noreply.github.com>
2022-09-22 15:00:08 +0530

    MatrixTranspose
    MatrixTranspose

---
+++ MatrixTranspose.java
@@ -0,0 +1,17 @@
+class MatrixTranspose
+{  
+  public static void main(String args[])
+{  
+
+int original[][]={{1,2,3},{4,5,6},{7,8,9}};//creating matrix and asigning values
+  
+for(int i=0;i<3;i++) //for Matrix Transpose
+{    
+for(int j=0;j<3;j++)
+{    
+System.out.print(original[j][i]+" ");    
+}    
+System.out.println();  //Printing Next line
+}    
+}
+}
